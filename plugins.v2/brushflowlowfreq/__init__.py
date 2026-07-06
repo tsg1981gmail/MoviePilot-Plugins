325320664aa9f97b93bb27aa0deffbb83d668dd0
yunbian <tsg1981@qq.com>
2026-07-06 10:24:25 +0800

    Bump brushflowlowfreq to 4.3.78

--- plugins.v2/brushflowlowfreq/__init__.py
+++ plugins.v2/brushflowlowfreq/__init__.py
@@ -565,7 +565,7 @@ class BrushFlowLowFreq(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "4.3.77"
+    plugin_version = "4.3.78"
     # 插件作者
     plugin_author = "jxxghp,InfinityPacer"
     # 作者主页
